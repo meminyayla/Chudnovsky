5e1c3f13b1b27233039efd3245b59e1cb95f26a1
meminyayla <m.eminyayla@gmail.com>
2022-04-27 23:11:20 +0300

    bazı yorumlar eklendi

--- Chudnovsky.py
+++ Chudnovsky.py
@@ -3,17 +3,17 @@ from decimal import Decimal, getcontext
 from math import ceil, factorial
 
 def pi(hassasiyet: int) -> str:
-    if not isinstance(hassasiyet, int):
+    if not isinstance(hassasiyet, int): #precision 
         raise TypeError("Tamsayı olmayanları için tanımsız / Undefined for non-integers")
     elif hassasiyet < 1:
         raise ValueError("Doğal olmayan sayılar için tanımsız / Undefined for non-natural numbers")
 
-    getcontext().prec = hassasiyet
-    interasyon_say = ceil(hassasiyet/14)
-    kalan = 426880 * Decimal(10005).sqrt()
-    ustel_terim = 1
-    lineer_terim = 13591409
-    parcali_toplam = Decimal(lineer_terim)
+    getcontext().prec = hassasiyet 
+    interasyon_say = ceil(hassasiyet/14) #number of iteration
+    kalan = 426880 * Decimal(10005).sqrt() #constant term
+    ustel_terim = 1 #exponential term
+    lineer_terim = 13591409 #linear term
+    parcali_toplam = Decimal(lineer_terim) #partial sum
     for k in range(1, interasyon_say):
         multinominal_terim = factorial(6 * k) // (factorial(3 * k) * factorial(k) **3)
         lineer_terim += 545140134
@@ -22,5 +22,5 @@ def pi(hassasiyet: int) -> str:
     return str(kalan / parcali_toplam)[:-1]
 
 if __name__ == "__main__":
-    n=50
-    print(f"Pi sayısının ilk {n} basamağı: {pi(n)}")
+    n=5000 ## Kaç basamak hesaplamak istediğinizi yazın - Write here how many digit you want to calculate.
+    print(f"Pi sayısının ilk {n} basamağı: {pi(n)}") 
